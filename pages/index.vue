fa83d2f6f65267dfb9009db93a1e80d2cdbbe4c4
pizzabote-32 <paul.hochleitner@outlook.com>
2024-04-07 17:20:44 +0200

    this is a commit for my pc

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,20 @@
 <template>
     <div>
-        <p>Paul hat das geschrieben</p>
+        <p>Paul hat das geschrieben: {{ text }}</p>
+
     </div>
 </template>
+<script>
+export default {
+    data(){
+        return {
+            text: ''
+        }
+    },
+  mounted(){
+    fetch('http://127.0.0.1:8055/items/Kommentar').then(data => data.json()).then(data => {
+        this.text = data.data[0].Titel
+    });
+  }
+}
+</script>
