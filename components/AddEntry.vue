23868d9546cca5aee9fb300c75c0847696fde58b
pizzabote-32 <paul.hochleitner@outlook.com>
2024-04-11 18:32:01 +0200

    implemantation of the add button for a post

--- components/AddEntry.vue
+++ components/AddEntry.vue
@@ -1,5 +1,7 @@
 <template>
-    <button class="absolute right-0 bottom-0 mb-12 mr-12 hover:bg-slate-300 py-3 px-3 rounded-md">
-        <img src="../pictures/plus.png" alt="plus">
-    </button>
+    <NuxtLink to="/random">
+        <button class="absolute right-0 bottom-0 mb-12 mr-12 hover:bg-slate-300 py-3 px-3 rounded-md">
+            <img src="../pictures/plus.png" alt="plus">
+        </button>
+    </NuxtLink>
 </template>
