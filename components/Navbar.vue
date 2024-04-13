225790b7a0e253a70165a07f256c106ee95b8ff3
pizzabote-32 <paul.hochleitner@outlook.com>
2024-04-13 13:14:18 +0200

    started implemanting the navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+    <nav class="px-52 py-11">
+        <div class="flex font-semibold text-xl items-center gap">
+            <div class="flex items-center gap-2 w-100">
+                <img src="../pictures/logo.png" alt="spengergasse-logo" class=" w-14" style="margin-right: auto">
+                <p>HTL Spengergasse</p>
+            </div>
+            <p>Kategorien</p>
+        </div>
+    </nav>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <Navbar />
         <AddPostButton/>
     </div>
 </template>
